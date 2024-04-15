2c6b5dbc8abbab0b4a4756b9a083e426cdd585ad
mukhtar011 <mukhtarazizullah79@gmail.com>
2024-04-15 17:23:05 +0500

    final commit

---
+++ lib/cart_model.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CartModel extends ChangeNotifier {
+  // list of items on sale
+  final List _shopItems = const [
+    // [ itemName, itemPrice, imagePath, color ]
+    ["Avocado", "300", "assets/images/avacado.png", Colors.green],
+    ["Banana", "150", "assets/images/banana.png", Colors.yellow],
+    ["Cherry", "200", "assets/images/cherry.png", Colors.brown],
+    ["Water", "100", "assets/images/water.png", Colors.blue],
+  ];
+
+  // list of cart items
+  List _cartItems = [];
+
+  get cartItems => _cartItems;
+
+  get shopItems => _shopItems;
+
+  // add item to cart
+  void addItemToCart(int index) {
+    _cartItems.add(_shopItems[index]);
+    notifyListeners();
+  }
+
+  // remove item from cart
+  void removeItemFromCart(int index) {
+    _cartItems.removeAt(index);
+    notifyListeners();
+  }
+
+  // calculate total price
+  String calculateTotal() {
+    double totalPrice = 0;
+    for (int i = 0; i < cartItems.length; i++) {
+      totalPrice += double.parse(cartItems[i][1]);
+    }
+    return totalPrice.toStringAsFixed(2);
+  }
+}
